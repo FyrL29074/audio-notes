e23276178d8f96e6c19f3e8b23eb744ed64f81ce
FyrL29074 <Fyrl29074@gmail.com>
2023-03-01 17:29:47 +0300

    init

---
+++ app/src/main/java/com/fyrl29074/audionotes/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fyrl29074.audionotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fyrl29074.audionotes.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/fyrl29074/audionotes/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.fyrl29074.audionotes.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
